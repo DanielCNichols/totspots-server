3663a982981f6e5622cd5e73c66702ecb8a34617
Daniel <herrnichols@gmail.com>
2019-10-15 13:55:12 -0400

    Add venue functionality is... added.

--- src/venues/venues-router.js
+++ src/venues/venues-router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const VenuesService = require('./venues-service');
-
+const path = require('path');
 const VenuesRouter = express.Router();
+const jsonBodyParser = express.json();
 
 VenuesRouter.route('/:city/:state/:type').get((req, res) => {
   VenuesService.getVenuesByCity(
@@ -20,6 +21,24 @@ VenuesRouter.route('/reviews/:venueId').get((req, res) => {
       res.json(reviews);
     }
   );
+});
+
+VenuesRouter.route('/').post(jsonBodyParser, (req, res, next) => {
+  const { venue_name, venue_type, address, city, state, zipcode } = req.body;
+  const newVenue = { venue_name, venue_type, address, city, state, zipcode };
+
+  for (const [key, value] of Object.entries(newVenue))
+    if (value === null) {
+      return res.status(400).json({ error: `Missing ${key} in request` });
+    }
+  VenuesService.addVenue(req.app.get('db'), newVenue)
+    .then(venue => {
+      res
+        .status(201)
+        .location(path.posix.join(req.originalUrl, `/${venue.id}`))
+        .json(VenuesService.serializeVenue(venue));
+    })
+    .catch(next);
 });
 
 module.exports = VenuesRouter;
--- src/venues/venues-service.js
+++ src/venues/venues-service.js
@@ -18,24 +18,34 @@ const VenuesService = {
       .then(([venue]) => venue);
   },
 
+  serializeVenue(newVenue) {
+    return {
+      id: newVenue.id,
+      venue_name: xss(newVenue.venue_name),
+      venue_type: xss(newVenue.venue_type),
+      address: xss(newVenue.address),
+      city: xss(newVenue.city),
+      state: xss(newVenue.state),
+      zipcode: xss(newVenue.zipcode)
+    };
+  },
+
   getReviewsByVenue(db, venue_id) {
-    return (
-      db
-        .from('reviews')
-        .select(
-          'reviews.id',
-          'reviews.content',
-          'reviews.price',
-          'reviews.starrating',
-          'reviews.volume',
-          'reviews.date_created',
-          'reviews.venue_id',
-          'reviews.user_id'
-        )
-        .where('reviews.venue_id', venue_id)
-        .join('totspots_users AS usr', 'reviews.user_id', 'usr.id')
-        .groupBy('reviews.id', 'usr.id')
-    );
+    return db
+      .from('reviews')
+      .select(
+        'reviews.id',
+        'reviews.content',
+        'reviews.price',
+        'reviews.starrating',
+        'reviews.volume',
+        'reviews.date_created',
+        'reviews.venue_id',
+        'reviews.user_id'
+      )
+      .where('reviews.venue_id', venue_id)
+      .join('totspots_users AS usr', 'reviews.user_id', 'usr.id')
+      .groupBy('reviews.id', 'usr.id');
   }
 };
 
